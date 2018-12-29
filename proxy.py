71d724bf2224c0505c4d1582fdbd7fdfce0e6fcd
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2018-12-29 13:45:15 +1300

    python3ify

--- proxy.py
+++ proxy.py
@@ -1,11 +1,13 @@
-import BaseHTTPServer
-import SocketServer
-import base64
-import httplib
-import shutil
-import urlparse
+#!/usr/bin/env python3
 
-basic = lambda u,p: 'Basic %s' % base64.b64encode('%s:%s' % (u,p))
+import http.server
+import socketserver
+import base64
+import http.client
+import shutil
+import urllib.parse
+
+basic = lambda u, p: 'Basic %s' % base64.b64encode(('%s:%s' % (u, p)).encode())
 
 AUTHORIZATIONS = {
     'maven.initech.com': basic('aladdin', 'opensesame'),
@@ -13,21 +15,22 @@ AUTHORIZATIONS = {
     'localhost:5000': basic('aladdin', 'opensesame'),
 }
 
-class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
+
+class Handler(http.server.BaseHTTPRequestHandler):
   def go(self):
-    ru = urlparse.urlparse(self.path)
-    pu = urlparse.ParseResult('', '', ru.path, ru.params, ru.query, ru.fragment)
+    ru = urllib.parse.urlparse(self.path)
+    pu = urllib.parse.ParseResult('', '', ru.path, ru.params, ru.query, ru.fragment)
     auth = AUTHORIZATIONS.get(str(ru.netloc))
     if auth:
       self.headers['Authorization'] = auth
     self.headers['Host'] = ru.netloc
     if ru.scheme == 'https':
-      c = httplib.HTTPSConnection(ru.netloc)
+      c = http.client.HTTPSConnection(ru.netloc)
     else:
-      c = httplib.HTTPConnection(ru.netloc)
+      c = http.client.HTTPConnection(ru.netloc)
     try:
       c.putrequest(self.command, pu.geturl())
-      for k, v in self.headers.items():
+      for k, v in list(self.headers.items()):
         c.putheader(k, v)
       c.endheaders()
       r = c.getresponse()
@@ -39,11 +42,14 @@ class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
       self.wfile.flush()
     finally:
       c.close()
+
   do_GET = go
   do_HEAD = go
 
-class ThreadedHTTPServer(SocketServer.ThreadingMixIn,
-                         BaseHTTPServer.HTTPServer):
+
+class ThreadedHTTPServer(socketserver.ThreadingMixIn,
+                         http.server.HTTPServer):
   daemon_threads = True
+
 
 ThreadedHTTPServer(('', 4000), Handler).serve_forever()
